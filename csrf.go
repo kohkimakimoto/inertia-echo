faa24cdef6dbf9383d9352ca5d9feb08738273cc
Kohki Makimoto <kohki.makimoto@gmail.com>
2022-02-16 17:18:22 +0900

    make CSRF middleware customizable

--- csrf.go
+++ csrf.go
@@ -1,16 +1,58 @@
 package inertia
 
 import (
+	"net/http"
+
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
 
 // CSRF is a middleware for protecting cross-site request forgery with Inertia.js
 // See: https://inertiajs.com/csrf-protection
-var CSRF = func() echo.MiddlewareFunc {
-	return middleware.CSRFWithConfig(middleware.CSRFConfig{
-		TokenLookup: "header:X-XSRF-TOKEN",
-		CookieName:  "XSRF-TOKEN",
-		CookiePath:  "/",
-	})
+
+type CSRFConfig middleware.CSRFConfig
+
+var DefaultCSRFConfig = CSRFConfig{
+	Skipper:        middleware.DefaultSkipper,
+	TokenLength:    32,
+	TokenLookup:    "header:X-XSRF-TOKEN",
+	ContextKey:     "csrf",
+	CookieName:     "XSRF-TOKEN",
+	CookieMaxAge:   86400,
+	CookieSameSite: http.SameSiteDefaultMode,
+	CookiePath:     "/",
+}
+
+func CSRF() echo.MiddlewareFunc {
+	c := DefaultCSRFConfig
+	return CSRFWithConfig(c)
+}
+
+func CSRFWithConfig(config CSRFConfig) echo.MiddlewareFunc {
+	if config.Skipper == nil {
+		config.Skipper = DefaultCSRFConfig.Skipper
+	}
+	if config.TokenLength == 0 {
+		config.TokenLength = DefaultCSRFConfig.TokenLength
+	}
+	if config.TokenLookup == "" {
+		config.TokenLookup = DefaultCSRFConfig.TokenLookup
+	}
+	if config.ContextKey == "" {
+		config.ContextKey = DefaultCSRFConfig.ContextKey
+	}
+	if config.CookieName == "" {
+		config.CookieName = DefaultCSRFConfig.CookieName
+	}
+	if config.CookieMaxAge == 0 {
+		config.CookieMaxAge = DefaultCSRFConfig.CookieMaxAge
+	}
+	if config.CookieSameSite == http.SameSiteNoneMode {
+		config.CookieSecure = true
+	}
+	if config.CookiePath == "" {
+		config.CookiePath = DefaultCSRFConfig.CookiePath
+	}
+
+	return middleware.CSRFWithConfig(middleware.CSRFConfig(config))
 }
--- csrf_test.go
+++ csrf_test.go
@@ -25,3 +25,41 @@ func TestCSRF(t *testing.T) {
 		t.Errorf("should contain XSRF-TOKEN, but not '%v'", cookie)
 	}
 }
+
+func TestCSRFWithConfig(t *testing.T) {
+	e := echo.New()
+	req := httptest.NewRequest(http.MethodGet, "/", nil)
+	rec := httptest.NewRecorder()
+	c := e.NewContext(req, rec)
+	csrf := CSRFWithConfig(CSRFConfig{
+		Skipper: func(c echo.Context) bool {
+			path := c.Request().URL.Path
+			if strings.HasPrefix(path, "/should_skip") {
+				return true
+			}
+			return false
+		},
+	})
+	h := csrf(func(c echo.Context) error {
+		return c.String(http.StatusOK, "test")
+	})
+	_ = h(c)
+
+	cookie := rec.Header().Get(echo.HeaderSetCookie)
+	if !strings.Contains(cookie, "XSRF-TOKEN") {
+		t.Errorf("should contain XSRF-TOKEN, but not '%v'", cookie)
+	}
+
+	req = httptest.NewRequest(http.MethodGet, "/should_skip", nil)
+	rec = httptest.NewRecorder()
+	c = e.NewContext(req, rec)
+	h = csrf(func(c echo.Context) error {
+		return c.String(http.StatusOK, "test")
+	})
+	_ = h(c)
+	cookie = rec.Header().Get(echo.HeaderSetCookie)
+	if strings.Contains(cookie, "XSRF-TOKEN") {
+		t.Errorf("should NOT contain XSRF-TOKEN because it should be skipped, but not '%v'", cookie)
+	}
+
+}
