e174c46bdfd6332b458fa98a9641455d92db97fe
Kohki Makimoto <kohki.makimoto@gmail.com>
2023-06-04 11:39:35 +0900

    wip

--- ssr/v8go/v8go.go
+++ ssr/v8go/v8go.go
@@ -3,19 +3,32 @@ package v8go
 import (
 	"github.com/kohkimakimoto/inertia-echo"
 	"rogchap.com/v8go"
+	"runtime"
+	"sync"
 )
 
 type SsrEngine struct {
+	Source string
+	Origin string
+	pool   sync.Pool
 }
 
 func NewSsrEngine() *SsrEngine {
-	return &SsrEngine{}
+	e := &SsrEngine{}
+	e.pool.New = func() interface{} {
+		// It is inspired by https://github.com/rogchap/v8go/issues/105#issuecomment-867332376
+		iso := v8go.NewIsolate()
+		runtime.SetFinalizer(iso, e.finalizer)
+		return iso
+	}
+	return e
 }
 
 func (e *SsrEngine) Render(p *inertia.Page) (*inertia.SsrResponse, error) {
 	return nil, nil
 }
 
-func (e *SsrEngine) newIsolate() *v8go.Isolate {
-	return v8go.NewIsolate()
+func (e *SsrEngine) finalizer(iso *v8go.Isolate) {
+	iso.Dispose()
+	runtime.SetFinalizer(iso, nil)
 }
