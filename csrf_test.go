d7dac50541c976a147fe93ca4846bea796b49098
Kohki Makimoto <kohki.makimoto@gmail.com>
2023-06-09 09:45:32 +0900

    update tests

--- csrf_test.go
+++ csrf_test.go
@@ -27,36 +27,47 @@ func TestCSRF(t *testing.T) {
 }
 
 func TestCSRFWithConfig(t *testing.T) {
-	e := echo.New()
-	req := httptest.NewRequest(http.MethodGet, "/", nil)
-	rec := httptest.NewRecorder()
-	c := e.NewContext(req, rec)
-	csrf := CSRFWithConfig(CSRFConfig{
-		Skipper: func(c echo.Context) bool {
-			path := c.Request().URL.Path
-			return strings.HasPrefix(path, "/should_skip")
-		},
-	})
-	h := csrf(func(c echo.Context) error {
-		return c.String(http.StatusOK, "test")
-	})
-	_ = h(c)
+	t.Run("has skipper but it should not skip", func(t *testing.T) {
+		e := echo.New()
+		req := httptest.NewRequest(http.MethodGet, "/", nil)
+		rec := httptest.NewRecorder()
+		c := e.NewContext(req, rec)
+		csrf := CSRFWithConfig(CSRFConfig{
+			Skipper: func(c echo.Context) bool {
+				path := c.Request().URL.Path
+				return strings.HasPrefix(path, "/should_skip")
+			},
+		})
+		h := csrf(func(c echo.Context) error {
+			return c.String(http.StatusOK, "test")
+		})
+		_ = h(c)
 
-	cookie := rec.Header().Get(echo.HeaderSetCookie)
-	if !strings.Contains(cookie, "XSRF-TOKEN") {
-		t.Errorf("should contain XSRF-TOKEN, but not '%v'", cookie)
-	}
-
-	req = httptest.NewRequest(http.MethodGet, "/should_skip", nil)
-	rec = httptest.NewRecorder()
-	c = e.NewContext(req, rec)
-	h = csrf(func(c echo.Context) error {
-		return c.String(http.StatusOK, "test")
+		cookie := rec.Header().Get(echo.HeaderSetCookie)
+		if !strings.Contains(cookie, "XSRF-TOKEN") {
+			t.Errorf("should contain XSRF-TOKEN, but not '%v'", cookie)
+		}
 	})
-	_ = h(c)
-	cookie = rec.Header().Get(echo.HeaderSetCookie)
-	if strings.Contains(cookie, "XSRF-TOKEN") {
-		t.Errorf("should NOT contain XSRF-TOKEN because it should be skipped, but not '%v'", cookie)
-	}
 
+	t.Run("has skipper and it should skip", func(t *testing.T) {
+		e := echo.New()
+		req := httptest.NewRequest(http.MethodGet, "/should_skip", nil)
+		rec := httptest.NewRecorder()
+		c := e.NewContext(req, rec)
+		csrf := CSRFWithConfig(CSRFConfig{
+			Skipper: func(c echo.Context) bool {
+				path := c.Request().URL.Path
+				return strings.HasPrefix(path, "/should_skip")
+			},
+		})
+		h := csrf(func(c echo.Context) error {
+			return c.String(http.StatusOK, "test")
+		})
+		_ = h(c)
+
+		cookie := rec.Header().Get(echo.HeaderSetCookie)
+		if strings.Contains(cookie, "XSRF-TOKEN") {
+			t.Errorf("should NOT contain XSRF-TOKEN because it should be skipped, but not '%v'", cookie)
+		}
+	})
 }
